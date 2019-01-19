88da304c0d92520e23af5534418ecb9c216777ea
kylemtam <43300149+kylemtam@users.noreply.github.com>
2019-01-19 08:45:48 -0500

    Api key added

--- Google Map API/Sample/googMaps.js
+++ Google Map API/Sample/googMaps.js
@@ -1,4 +1,4 @@
-let map, infoWindow;
+let map, infoWindow, marker, messagewindow;
 
 function initMap() {
 	let latitude, longitude;
@@ -39,3 +39,40 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 		'Error: Your browser is not supported.');
 	  infoWindow.open(map);
 }
+
+/*
+	 function saveData() {
+		 let Location = escape(document.getElementById('Location').value);
+		 let type = document.getElementById('type').value;
+		 let latlng = marker.getPosition();
+		 let url = 'phpsqlinfo_addrow.php?Location=' + Location +
+							 '&type=' + type + '&lat=' + latlng.lat() + '&lng=' + latlng.lng();
+
+		 downloadUrl(url, function(data, responseCode) {
+
+			 if (responseCode == 200 && data.length <= 1) {
+				 infowindow.close();
+				 messagewindow.open(map, marker);
+			 }
+		 });
+	 }
+
+	 function downloadUrl(url, callback) {
+		 let request = window.ActiveXObject ?
+				 new ActiveXObject('Microsoft.XMLHTTP') :
+				 new XMLHttpRequest;
+
+		 request.onreadystatechange = function() {
+			 if (request.readyState == 4) {
+				 request.onreadystatechange = doNothing;
+				 callback(request.responseText, request.status);
+			 }
+		 };
+
+		 request.open('GET', url, true);
+		 request.send(null);
+	 }
+
+	 function doNothing () {
+	 }
+	 */
